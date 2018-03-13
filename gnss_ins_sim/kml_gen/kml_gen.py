58e90fbf410757e54ae24d5c12ad7fa61bd50030
dxg-aceinna <xgdong@aceinna.com>
2018-03-13 16:37:46 +0800

    Now we can generate .kml files from both [Lat Lon Alt] and [x y z]

--- gnss_ins_sim/kml_gen/kml_gen.py
+++ gnss_ins_sim/kml_gen/kml_gen.py
@@ -7,27 +7,58 @@ Created on 2017-11-29
 @author: dongxiaoguang
 """
 
+import os
 import math
 import numpy as np
+from ..geoparams import geoparams
+from ..attitude import attitude
 
 R2D = 180.0/math.pi
 
-def kml_gen_from_lla(lla, template_file):
+def kml_gen(pos, template_file='template.kml', convert_to_lla=False):
     '''
-    Generate .kml file contents from lla.
+    Generate .kml file contents from position data.
     Args:
-        lla: nx3 [lat, lon, alt], deg, m.
+        pos: nx3 [lat, lon, alt in rad and m, or [x, y, z] in m
         template_file: template kml file. The line 'REPALCE THIS WITH COORDINATES'
-                       will be replaced by coordinates defined in lla.
+                       will be replaced by coordinates defined in pos.
     Returns:
         kml_contents: string contents of kml file.
     '''
+    # get lla from pos
+    n = pos.shape[0]
+    if convert_to_lla is False:
+        lla = pos
+        lla[:, 0] = lla[:, 0] * R2D
+        lla[:, 1] = lla[:, 1] * R2D
+    else:
+        lla = np.zeros(pos.shape)
+        # # xyz to lla
+        # for i in range(0, n):
+        #     lla[i, :] = geoparams.xyz2lla(pos[i, :])
+        # # rad to deg
+        # lla[:, 0] = lla[:, 0] * R2D
+        # lla[:, 1] = lla[:, 1] * R2D
+        # ned to lla
+        lla[0, :] = geoparams.xyz2lla(pos[0, :])
+        ecef_to_ned = attitude.rot_y(-math.pi/2.0-lla[0, 0]).dot(attitude.rot_z(lla[0, 1]))
+        for i in range(1, n):
+            ned_pos = pos[i, :]-pos[0, :]
+            ecef_pos = pos[0, :] + ecef_to_ned.T.dot(ned_pos)
+            lla[i, :] = geoparams.xyz2lla(ecef_pos)
+        # rad to deg
+        lla[:, 0] = lla[:, 0] * R2D
+        lla[:, 1] = lla[:, 1] * R2D
     # gen kml according to data and template
+    if template_file is None:
+        template_file = os.path.join(os.path.dirname(__file__), 'template.kml')
+    else:
+        if not os.path.isabs(template_file):     # add by DXG
+            template_file = os.path.join(os.path.dirname(__file__), template_file)
     fp = open(template_file)
     lines = ''
     for line in fp:
         if line == 'REPALCE THIS WITH COORDINATES\n':
-            n = lla.shape[0]
             for i in range(0, n):
                 if i == 0:
                     lines = lines + '\t\t\t\t'

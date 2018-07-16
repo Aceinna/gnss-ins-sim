e81d6ef3623e0322bff4f2abe98c8e1ead377ef7
dxg-aceinna <xgdong@aceinna.com>
2018-07-16 14:10:19 +0800

    Update mag calibration algorithm

--- demo_mag_cal.py
+++ demo_mag_cal.py
@@ -30,7 +30,7 @@ def test_mag_cal():
     imu_err = 'mid-accuracy'
     # do not generate GPS data
     imu = imu_model.IMU(accuracy=imu_err, axis=9, gps=False)
-    mag_error = {'si': np.eye(3) + np.random.randn(3, 3)*0.0,
+    mag_error = {'si': np.eye(3) + np.random.randn(3, 3)*0.1,
                  'hi': np.array([10.0, 10.0, 10.0])*1.0
                 }
     imu.set_mag_error(mag_error)
@@ -40,7 +40,7 @@ def test_mag_cal():
 
     #### start simulation
     sim = ins_sim.Sim([fs, fs_gps, fs_mag],
-                      motion_def_path+"//motion_def_mag_cal2.csv",
+                      motion_def_path+"//motion_def_mag_cal.csv",
                     #   motion_def_path+"//test_mag_cal//",
                       ref_frame=1,
                       imu=imu,
@@ -49,7 +49,7 @@ def test_mag_cal():
                       algorithm=algo)
     sim.run(1)
     # save simulation data to files
-    sim.results('aa')
+    sim.results()
     # plot data
     sim.plot(['mag', 'mag_cal'], opt={'mag': 'projection', 'mag_cal': 'projection'}, extra_opt='.')
     # show calibration params:

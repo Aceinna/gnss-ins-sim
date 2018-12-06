8d834531b780981c0c9a0bb26c6df1cd0548db7a
Dong Xiaoguang <xgdong@aceinna.com>
2018-12-06 16:12:33 +0800

    fix a bug when convert lla from deg to rad

--- demo_free_integration_openimu.py
+++ demo_free_integration_openimu.py
@@ -32,7 +32,7 @@ def test_free_integration():
     # add initial states error if needed
     ini_vel_err = np.array([0.0, 0.0, 0.0]) # initial velocity error in the body frame, m/s
     ini_att_err = np.array([0.0, 0.0, 0.0]) # initial Euler angles error, deg
-    ini_pos_vel_att[0:3] = ini_pos_vel_att[0:3] * attitude.D2R
+    ini_pos_vel_att[0:2] = ini_pos_vel_att[0:2] * attitude.D2R
     ini_pos_vel_att[3:6] += ini_vel_err
     ini_pos_vel_att[6:9] = (ini_pos_vel_att[6:9] + ini_att_err) * attitude.D2R
     if not using_external_g:
@@ -51,7 +51,7 @@ def test_free_integration():
     # run the simulation
     sim.run(1)
     # generate simulation results, summary
-    sim.results('', end_point=False)
+    sim.results('', end_point=False, gen_kml=True)
     # plot
     sim.plot(['pos', 'vel', 'att_euler', 'accel'],\
              opt={'pos':'error', 'vel':'error', 'att_euler': 'error'},\

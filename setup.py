27d3ae23dad883327ddbccd4fd72be26431132ec
Jim Ren <jimren@fb.com>
2019-06-03 17:35:56 -0700

    Added setup.py file for pip install

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gnss-ins-sim",
+    version="2.1",
+    author="Aceinna",
+    author_email="xgdong@aceinna.com",
+    description="GNSS-INS-SIM is an GNSS/INS simulation project, "
+        "which generates reference trajectories, IMU sensor output, "
+        "GPS output, odometer output and magnetometer output.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Aceinna/gnss-ins-sim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
